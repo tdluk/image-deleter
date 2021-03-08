041b5fa637bb8f029c5638a8a786572fd6c89024
Theo Lukens <theolukens@hotmail.co.uk>
2021-03-08 18:42:29 +0000

    Get script working

--- deleter.py
+++ deleter.py
@@ -12,17 +12,14 @@ import json
 import requests
 import argparse
 
-# import config
-# api_root = config.api_root
-
-def delete_single_tag(repo, tag):
-    """ Schedules a single image tag for deletion using registry API.
+def delete_single_tag(api_root, repo, tag):
+    """ Marks a single image tag for deletion using registry API.
     """
     request = requests.get('{}/{}/manifests/{}'.format(api_root, repo, tag), 
         headers={'Accept': 'application/vnd.docker.distribution.manifest.v2+json'})
     if request.status_code == 200: # OK
             digest = request.headers['Docker-Content-Digest']
-            print("DELETING TAG: {} {}".format(repo, tag))
+            print("DELETING TAG {}:{}".format(repo, tag))
             print("DIGEST: {}".format(digest))
             delete = requests.delete('{}/{}/manifests/{}'.format(api_root, repo, digest),
                 headers={'Accept': 'application/vnd.docker.distribution.manifest.v2+json'},
@@ -32,23 +29,27 @@ def delete_single_tag(repo, tag):
             else:
                 print("Failed to delete {}".format(digest))
     elif request.status_code == 404: # NOT FOUND
-        print("Couldn't find tag: {} {}".format(repo, tag))
+        print("Couldn't find tag {}:{}".format(repo, tag))
     else:
-        print("Tag {} {} returned code: {}".format(repo, tag, request.status_code))
+        print("Tag {}:{} returned code {}".format(repo, tag, request.status_code))
 
 def parse_script_args():
     """
     """
     parser = argparse.ArgumentParser(description="Delete images from filesystem.")
-    parser.add_argument('--api_root', type=str, nargs=1, required=True,
-                        help='API root e.g. http://1.2.3.4:5000/v2')
+    parser.add_argument('--reg_ip', type=str, required=True,
+                        help='Registry host address e.g. 1.2.3.4')
     parser.add_argument('images', type=str, nargs='+',
                         help='Images to delete in the form repo:tag, e.g. centos:latest')
     args = parser.parse_args()
-    # TODO split images into repo and tag
+    for image in args.images:	
+        if image.count(':') != 1:
+            parser.error("Malformed image(s)")
     return args
 
 if __name__ == '__main__':
     args = parse_script_args()
-    # TODO
-
+    api_root = "http://{}:5000/v2".format(args.reg_ip)
+    for image in args.images:
+        repo, tag = image.split(':')
+        delete_single_tag(api_root, repo, tag)
